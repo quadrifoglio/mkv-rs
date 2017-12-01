2c2f182022abf598c894d074ab79f74078fd7774
quadrifoglio <quadrifoglio.clement@protonmail.com>
2017-12-01 22:38:27 +0100

    segment information reading

--- src/reader/segment.rs
+++ src/reader/segment.rs
@@ -3,12 +3,25 @@
 use std::io::Read;
 use std::collections::HashMap;
 
+use ebml::element::types::*;
 use ebml::element::Id as EbmlId;
-use ebml::reader::{Reader, ReadElement};
+use ebml::reader::Reader;
 
 use error::{Error, ErrorKind, Result};
 use elements as el;
 
+/// A type alias representing a map of Element IDs to their position in the MKV file.
+pub type SeekEntries = HashMap<EbmlId, UnsignedInt>;
+
+/// Contains parsed information about an MKV segment.
+#[derive(Default)]
+pub struct SegmentInfo {
+    pub uid: Option<Binary>,
+    pub filename: Option<Utf8>,
+    pub timecode_scale: UnsignedInt
+}
+
+/// Initialize the specified EBML reader to make it ready to read MKV segment information.
 pub fn init<R: Read>(ebml: &mut Reader<R>) {
     // Meta Seek Information.
     ebml.register::<el::SeekHead>();
@@ -37,7 +50,8 @@ pub fn init<R: Read>(ebml: &mut Reader<R>) {
     ebml.register::<el::WritingApp>();
 }
 
-pub fn read_seek_information<R: Read>(ebml: &mut Reader<R>) -> Result<HashMap<EbmlId, u64>> {
+/// Read seeking information. Returns a map of Elements to their position in the file.
+pub fn read_seek_information<R: Read>(ebml: &mut Reader<R>) -> Result<SeekEntries> {
     let mut entries = HashMap::new();
     let (elem, _) = ebml.read_element(true)?;
 
@@ -49,4 +63,26 @@ pub fn read_seek_information<R: Read>(ebml: &mut Reader<R>) -> Result<HashMap<Eb
     }
 
     Ok(entries)
+}
+
+/// Read segment information.
+pub fn read_information<R: Read>(ebml: &mut Reader<R>) -> Result<SegmentInfo> {
+    let mut segment_info = SegmentInfo::default();
+    let (elem, _) = ebml.read_element(true)?;
+
+    if let Some(uid) = elem.find::<el::SegmentUID>() {
+        segment_info.uid = Some(uid.data().clone().take()?);
+    }
+
+    if let Some(filename) = elem.find::<el::SegmentFilename>() {
+        segment_info.filename = Some(filename.data().to_utf8()?);
+    }
+
+    if let Some(timecode_scale) = elem.find::<el::TimecodeScale>() {
+        segment_info.timecode_scale = timecode_scale.data().to_unsigned_int()?;
+    } else {
+        segment_info.timecode_scale = 1000000;
+    }
+
+    Ok(segment_info)
 }
